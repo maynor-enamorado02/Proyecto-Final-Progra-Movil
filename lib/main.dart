b450f71e09c9865a71a1278613e8b7c9882ebb4f
andresteamrbdg <andresedu1996@hotmail.com>
2025-06-12 12:32:54 -0600

    V9
    Test

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class PokemonApp extends StatelessWidget {
       builder: (context, themeProvider, _) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
-          title: 'PokéStats',
+          title: 'PokeStats',
           theme: themeProvider.themeData,
           home: FirebaseAuth.instance.currentUser == null
               ? const LoginPage()
